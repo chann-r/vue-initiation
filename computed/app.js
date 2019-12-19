3b264cb304ad6c06d2926730c3e22cbbfc2b3526
hysrtr <hys.rtr.forit>
2019-12-19 15:20:19 +0900

    Add：文房具の情報を追加

---
+++ computed/app.js
@@ -0,0 +1,17 @@
+var items = [
+  {
+    name: '鉛筆',
+    price: 50,
+    quantity: 1
+  }
+  {
+    name: 'ノート',
+    price: 90,
+    quantity: 2
+  }
+  {
+    name: '消しゴム',
+    price: 100,
+    quantity: 1
+  }
+]
