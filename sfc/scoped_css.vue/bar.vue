52ecd605247a73b61fe5869747f17787746c00f2
hysrtr <hys.rtr.forit>
2019-12-30 22:43:10 +0900

    Add：別のコンポーネントをインポートして登録し、カスタムタグで利用

---
+++ sfc/scoped_css.vue/bar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bar">
+    <h1 class="header">Barコンポーネント</h1>
+    <p>これはBarコンポーネントです。</p>
+    <!-- インポートして登録したFooコンポーネントを利用 -->
+    <foo></foo>
+
+  </div>
+</template>
+
+<script>
+// foo.vueのコンポーネントをインポート（インポートしてFooコンポーネントと名付ける）
+import Foo from './foo'
+export default {
+  // Fooコンポーネントをこのファイルのコンポーネントに登録
+  components: { Foo }
+}
+</script>
+
+<style scoped>
+.bar {
+  border: 1px solid red;
+  margin: 4px;
+  padding: 4px;
+}
+
+.header {
+  font-size: 125%;
+}
+</style>
