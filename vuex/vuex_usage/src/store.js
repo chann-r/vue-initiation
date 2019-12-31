1bcf99d8690ef89537d8ff9adb558b7cfeb28683
hysrtr <hys.rtr.forit>
2019-12-31 23:26:03 +0900

    Add：ミューテーションを追加

--- vuex/vuex_usage/src/store.js
+++ vuex/vuex_usage/src/store.js
@@ -7,6 +7,7 @@ const store = new Vuex.Store({
   // アプリケーション全体で使用されるデータの状態を管理するためのステート
   state: {
     // タスクの初期ステート
+    // {} ではなく、[]
     tasks: [
       {
         id: 1,
@@ -18,7 +19,31 @@ const store = new Vuex.Store({
         name: 'Vue.jsの本を買う',
         done: true
       }
-    ]
+    ],
+    nextTaskId: 3
+  },
+  // mutationsは、ステートを更新するためのもの
+  mutations: {
+    // addTaskミューテーションを定義
+    // 第一引数に渡されたステートを更新する
+    // 第二引数に
+    // ミューテーションは直接は呼び出せず、store.commitにミューテーション名を与えて呼ぶ出す
+    addTask (state, { name }) {
+      state.tasks.push({
+        id: state.nextTaskId,
+        name,
+        done: false
+      })
+      state.nextTaskId++
+    },
+    toggleTaskStatus (state, { id }) {
+      const filtered = state.tasks.filter(task => {
+        return task.id === id
+      })
+      filterd.forEach(task => {
+        task.done = !task.done
+      })
+    }
   }
 })
 
