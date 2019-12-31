959fa21f00f549e3685b3ade8f97edd9f5f6d28a
hysrtr <hys.rtr.forit>
2019-12-31 17:12:45 +0900

    Add；ページで表示できるように算出プロパティとテンプレートを設定

--- vuex/sample/App.vue
+++ vuex/sample/App.vue
@@ -2,12 +2,26 @@
 
 <template>
   <div>
-
+    <h2>タスク一覧</h2>
+    <ul>
+      <li v-for="task in tasks" v-bind:key="task.id">
+        <input type="checkbox" v-bind:checked="task.done">
+        {{ task.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
+
 <script>
 export default {
+  // テンプレート内でストアで定義したステートを利用できるように
+  // 算出プロパティでストアのステートを返すようにする
+  computed: {
+    tasks () {
+      return this.$store.state.tasks // ストアを読む
+    }
+  }
 }
 </script>
 
