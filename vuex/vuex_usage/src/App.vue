b8a51d9aa4365d3adb840652c5a4d5973868d7c6
hysrtr <hys.rtr.forit>
2019-12-31 23:47:42 +0900

    Add：storeで定義したミューテーションをコンポーネントで利用できるようにメソッドとして再定義

--- vuex/vuex_usage/src/App.vue
+++ vuex/vuex_usage/src/App.vue
@@ -5,21 +5,44 @@
     <h2>タスク一覧</h2>
     <ul>
       <li v-for="task in tasks" v-bind:key="task.id">
-        <input type="checkbox" v-bind:checked="task.done">
+        <!-- changeイベントによってtoggleTaskStatusミューテーションを呼び出す -->
+        <input type="checkbox" v-bind:checked="task.done" v-on:change="toggleTaskStatus(task)">
         {{ task.name }}
       </li>
     </ul>
+    <form v-on:submit.prevent="addTask">
+      <input type="text" v-model="newTaskName" placeholder="新しいタスク">
+
+    </form>
   </div>
 </template>
 
 
 <script>
 export default {
+  data () {
+    return {
+      newTaskName: ''
+    }
+  },
   // テンプレート内でストアで定義したステートを利用できるように
   // 算出プロパティでストアのステートを返すようにする
   computed: {
     tasks () {
       return this.$store.state.tasks // ストアを読む
+    }
+  },
+  methods: {
+    addTask () {
+      this.$store.commit('addTask', {
+        name: this.newTaskName
+      })
+      this.newTaskName = ''
+    },
+    toggleTaskStatus (task) {
+      this.$store.commit('toggleTaskStatus', {
+        id: task.id
+      })
     }
   }
 }
